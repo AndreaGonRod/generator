2060349e98b284248506e524e3b4250a27c8088a
Andie Morke <morkeandie@gmail.com>
2026-09-23 15:23:28 +0200

    feat: add PWA support using vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.svg'],
+      manifest: {
+        name: 'Generador de Nombres',
+        short_name: 'Nombres',
+        description: 'Generador morfológico y semántico de nombres clásicos y míticos',
+        theme_color: '#080a10',
+        background_color: '#080a10',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: '/nomen/'
 });
